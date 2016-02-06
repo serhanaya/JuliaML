d1c9c0384b5df68eee32b113d6479b23d404790d
Serhan <serhane@gmail.com>
2016-02-06 11:35:33 +0200

    Update Training.jl
    All methods are written. (TODO: NeuralNetTest.jl)

--- NeuralNetwork/src/train/Training.jl
+++ NeuralNetwork/src/train/Training.jl
@@ -9,7 +9,8 @@ type Training
     end
 end
 
-# TrainingTypesENUM
+@enum TrainingTypesENUM PERCEPTRON ADALINE
+@enum ActivationFncENUM STEP LINEAR SIGLOG HYPERTAN
 
 function train(n::NeuralNet, t::Training)
     inputWeightIn = Vector{Float64}(0)
@@ -33,12 +34,12 @@ function train(n::NeuralNet, t::Training)
             realOutput = n.realOutputSet[i] 
             t.error = realOutput - estimatedOutput
             
-            if abs(t.error > n.targetError
+            if abs(t.error) > n.targetError
                 # fix weights
-                inpLayer = InputLayer()
-                inpLayer.listOfNeurons = teachNeuronsOfLayer(cols, i,
-                    n, netValue)
-                n.InputLayer = inpLayer
+                iLayerTemp = 
+                    InputLayer(listOfNeurons = teachNeuronsOfLayer(cols, i,
+                        n, netValue))  # Fix constructor of InputLayer.
+                n.iLayer = iLayerTemp
             end
         end
 
@@ -76,6 +77,84 @@ function teachNeuronsOfLayer(numberOfInputNeurons::Int64, line::Int64,
 end
 
 
+function calcNewWeight(trainType::TrainingTypesENUM, inputWeightOld::Float64,
+    n::NeuralNet, error::Float64, trainSample::Float64, netValue::Float64)
+    
+    if trainType == PERCEPTRON
+        return inputWeightOld + n.learningRate * error * trainSample
+    elseif trainType == ADALINE
+        return inputWeightOld * n.learningRate * error * trainSample *
+             derivativeActivationFnc(n.activationFncType, netValue)
+    else
+        throw(ArgumentError(trainType
+                    + " does not exist in TrainingTypesENUM"))
+    end
+end
 
+function activationFnc(fnc::ActivationFncENUM, value::Float64)
+    
+    if fnc == STEP
+        if value >= 0
+            return 1.0
+        else
+            return 0.0
+        end
+    
+    elseif fnc == LINEAR
+        return value
+    
+    elseif fnc == SIGLOG
+        return 1.0 / (1.0 + exp(-value))
 
+    elseif fnc == HYPERTAN
+        return tanh(value)
 
+    else
+        throw(ArgumentError(fnc
+                    + " does not exist in ActivationFncENUM"))
+    
+    end
+end
+
+function derivativeActivationFnc(fnc::ActivationFncENUM, value::Float64)
+    
+    if fnc == LINEAR
+        return 1.0
+    
+    elseif fnc == SIGLOG
+        return value * (1.0 - value);
+
+    elseif fnc == HYPERTAN
+        return 1.0 / cosh(value)^2
+
+    else
+        throw(ArgumentError(fnc
+                    + " does not exist in ActivationFncENUM"))
+    
+    end
+end    
+
+function printTrainedResult(trainedNet::NeuralNet, t::Training)
+    rows = size(trainedNet.trainSet)[1]
+    cols = size(trainedNet.trainSet)[2]
+
+    inputWeightIn = Vector{Float64}(0)
+
+    for i = 1:rows
+        netValue = 0.0
+        for j = 1:cols
+            inputWeightIn = trainedNet.iLayer.listOfNeurons[j].listOfWeightIn
+            inputWeight = inputWeightIn[1]
+            netValue = netValue + inputWeight * trainedNet.trainSet[i,j]
+            print(trainedNet.trainSet[i,j] + "\t")  # May cause a problem
+        end
+
+        estimatedOutput = activationFnc(trainedNet.trainSet[i,j])
+        print(trainedNet.trainSet[i,j] + "\t")  # Change with @printf
+        print(" REAL OUTPUT: "
+                    + trainedNet.realOutputSet[i] + "\t")
+        error = estimatedOutput - trainedNet.realOutputSet[i]
+        print(" ERROR: " + error + "\n")
+
+    end
+end
